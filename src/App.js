a5545a2d8faaf12a98358cc366efedb810a7bd0a
Satish980 <isatishkumar4008@gmail.com>
2022-10-02 11:16:50 +0530

    show case website completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="You own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, storem collect NFTs, exchange & earn crypto.
+        Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interfaxe MarketPlace"
+        description="Experience a buttery UI of ProNef NFT MarketPlace. 
+        Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deplyment"
+        description="ProNef is built using Expo which runs natively on all users devices. You can easily get yoir app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center flex-col bg-primary text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className="bold">Satish Kumar</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
